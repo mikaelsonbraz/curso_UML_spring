5468b8dd1e2f49787dad72b2279182946feae356
mikaelsonbraz <mikaelsonmrn@gmail.com>
2022-02-24 16:56:27 -0300

    consertando, Endereço estava sem o atributo identificador <pk>

--- src/main/java/com/mikaelsonbraz/cursomc/domain/Endereco.java
+++ src/main/java/com/mikaelsonbraz/cursomc/domain/Endereco.java
@@ -4,11 +4,16 @@ import java.io.Serializable;
 import java.util.Objects;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class Endereco implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String logradouro;
 	private String numero;
