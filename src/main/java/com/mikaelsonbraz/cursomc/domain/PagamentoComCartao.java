26a0a7fd752d65bae8752a21862677a2ad8b0ce2
mikaelsonbraz <mikaelsonmrn@gmail.com>
2022-02-25 13:02:48 -0300

    Domínios para as classes Pagamento (e suas subclasses) e Pedido criados, assim como os repositórios e o enum para o EstadoPagamento

---
+++ src/main/java/com/mikaelsonbraz/cursomc/domain/PagamentoComCartao.java
@@ -0,0 +1,32 @@
+package com.mikaelsonbraz.cursomc.domain;
+
+import javax.persistence.Entity;
+
+import com.mikaelsonbraz.cursomc.domain.enums.EstadoPagamento;
+
+@Entity
+public class PagamentoComCartao extends Pagamento{
+	private static final long serialVersionUID = 1L;
+	
+	private Integer numParcelas;
+	
+	public PagamentoComCartao() {
+	}
+
+	public PagamentoComCartao(Integer id, EstadoPagamento estadoPagamento, Pedido pedido, Integer numParcelas) {
+		super(id, estadoPagamento, pedido);
+		this.numParcelas = numParcelas;
+	}
+
+	public Integer getNumParcelas() {
+		return numParcelas;
+	}
+
+	public void setNumParcelas(Integer numParcelas) {
+		this.numParcelas = numParcelas;
+	}
+
+	
+	
+
+}
